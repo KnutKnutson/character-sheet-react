6f38fc64b4ed8e21c12b2a53716c58e3797fd360
Mark Knutson <knutson.mc@gmail.com>
2016-04-05 20:14:57 -0500

    update build scripts, readme, tools

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # character-sheet-react
+
+A character sheet app built in JavaScript with React and Firebase.
+
+## Usage
+
+## Development environment setup
+
+0. Install [NodeJS](https://nodejs.org/en/)
+0. `npm install`
+0. `npm run build`
+
+### Starting the dev server
+
+To start a dev server: `npm start`. Site will be available at http://localhost:8080/webpack-dev-server/
+
+### Running Tests
+
+Coming soon!
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    context: __dirname,
+    entry: {
+        app: './src/js/app.js',
+        vendor: ['react', 'react-dom', 'firebase']
+    },
+    output: {
+        path:     'build',
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js')
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                include: [
+                    path.join(__dirname, 'src')
+                ],
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                include: [
+                    path.join(__dirname, 'src')
+                ],
+                loader: 'style!css'
+            }
+        ]
+    },
+    devtool: 'source-map'
+};
